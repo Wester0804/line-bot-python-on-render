bfe4f67de927a812b6ba1b5468dfe831edefa89d
Wester0804 <128884444+Wester0804@users.noreply.github.com>
2023-03-25 18:09:57 +0800

    Add files via upload

---
+++ line_bot.py
@@ -0,0 +1,16 @@
+from flask_ngrok import run_with_ngrok   # colab 
+from flask import Flask, request
+import json
+
+app = Flask(__name__)
+
+@app.route("/", methods=['POST'])
+def linebot():
+    body = request.get_data(as_text=True)
+    json_data = json.loads(body)
+    print(json_data)               
+    return 'OK'
+
+if __name__ == "__main__":
+    run_with_ngrok(app)        # colab 
+    app.run()
